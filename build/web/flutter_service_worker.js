c668f7ebbc576adf851e6345cef35b6608703d35
MusabYK <musabyakubu1000@gmail.com>
2025-08-13 01:38:23 +0100

    new release

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "48152201980eb274f29b5c9ccb3af700
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "6d2c31545676f68455007698f53e48ff",
+"flutter_bootstrap.js": "998c9b82a63b3146d8d4de627ed0a205",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
